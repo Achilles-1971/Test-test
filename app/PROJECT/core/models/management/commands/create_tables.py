9bdc7155b7ba149c3c63f4ba3fd48c20377dd1f2
Anton Knyazev <knyazevmac@icloud.com>
2024-12-10 13:56:02 +0400

    Обновлен файл с созданием таблиц в базе данных. Теперь, при создании таблиц, у нас создаются и роли в базе данных

--- app/PROJECT/core/models/management/commands/create_tables.py
+++ app/PROJECT/core/models/management/commands/create_tables.py
@@ -1,21 +1,25 @@
 from django.core. management.base import BaseCommand
 from models.models import Base, Role
 from models.config import session
+from sqlalchemy.sql import select
 class Command(BaseCommand):
     help = "Эта команда создает таблицы в базе данных"
     def handle(self, *args, **options):
         try:
             Base. metadata.create_all(session.get_bind())
             # Создаем роли
-            roles = [
-                Role(name="пользователь"),
-                Role(name="организатор"),
-                Role(name="жюри"),
-                Role(name="модератор"),
-            ]
-            # Сохраняем роли в базу данных
-            session.add_all(roles)
-            session.commit()
+            roles_sql_row = select(Role)
+            roles = session.scalars(roles_sql_row).all()
+            if len(roles) == 0:
+                roles = [
+                    Role(name="пользователь"),
+                    Role(name="организатор"),
+                    Role(name="жюри"),
+                    Role(name="модератор"),
+                ]
+                # Сохраняем роли в базу данных
+                session.add_all(roles)
+                session.commit()
             self.stdout.write(self.style.SUCCESS("Таблицы успешно созданы!"))
         except Exception as e:
             self.stderr.write(self.style.ERROR(f"Ошибка при создании таблиц: {e}"))
